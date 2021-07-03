5c6a6e4dfe012157cc0486fda0482918bbe04d23
Leon Buikstra <boksha@hotmail.com>
2021-07-03 16:52:13 +0200

    Make node draggable

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,63 @@
-let myImage = document.querySelector('img');
+function addDragHandlersToSvg(evt) {
+  console.log("Henlo.");
 
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/test-image.png') {
-      myImage.setAttribute('src','images/test-image2.png');
-    } else {
-      myImage.setAttribute('src','images/test-image.png');
+  let svg = evt.target;
+
+  let draggedElement = null;
+  let draggedOffsetInSvgCoordinates = null;
+
+  function getMousePositionInSvgCoordinates(evt) {
+    var svgCtm = svg.getScreenCTM();
+    return {
+      x: (evt.clientX - svgCtm.e)/svgCtm.a,
+      y: (evt.clientY - svgCtm.f)/svgCtm.d,
+    };
+  }
+
+  function getDraggableGroupPositionInSvgCoordinates(draggable) {
+    let svgCtm = svg.getScreenCTM();
+    let draggableCtm = draggable.getScreenCTM();
+    return {
+      x: (draggableCtm.e - svgCtm.e)/svgCtm.a,
+      y: (draggableCtm.f - svgCtm.f)/svgCtm.d,
     }
+  }
+
+  function startDrag(evt) {
+    let dragHandle = evt.target.closest('.drag_handle');
+    if (dragHandle) {
+      draggedElement = dragHandle.closest('.draggable');  // A drag_handle must be nested in a draggable
+      let mousePosition = getMousePositionInSvgCoordinates(evt);
+      let draggablePosition = getDraggableGroupPositionInSvgCoordinates(draggedElement);
+      draggedOffsetInSvgCoordinates = {
+        x: draggablePosition.x - mousePosition.x,
+        y: draggablePosition.y - mousePosition.y,
+      };
+    }
+  }
+
+  function drag(evt) {
+    if (draggedElement) {
+      // Note: draggable elements must ONLY have a translate transform
+      evt.preventDefault();
+      let mousePosition = getMousePositionInSvgCoordinates(evt);
+      let newPosition = {
+        x: mousePosition.x + draggedOffsetInSvgCoordinates.x,
+        y: mousePosition.y + draggedOffsetInSvgCoordinates.y,
+      };
+      draggedElement.setAttributeNS(null, "transform", "translate(" + newPosition.x + "," + newPosition.y + ")");
+    }
+  }
+
+  function endDrag(evt) {
+    if (draggedElement) {
+      draggedElement = null;
+    }
+  }
+
+  svg.addEventListener('mousedown', startDrag);
+  svg.addEventListener('mousemove', drag);
+  svg.addEventListener('mouseup', endDrag);
+  svg.addEventListener('mouseleave', endDrag);
 }
+
